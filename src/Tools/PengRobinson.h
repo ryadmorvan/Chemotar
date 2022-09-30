d06c1c988d3be523fca382d9cff577c0436b40fb
Ryad M <48857076+ryadmorvan@users.noreply.github.com>
2022-09-30 13:49:41 -0700

    Add files via upload

--- src/Tools/PengRobinson.h
+++ src/Tools/PengRobinson.h
@@ -1,5 +1,6 @@
 #pragma once
 #include <imgui.h>
+#include <math.h>
 
 
 class PengRobinson
@@ -8,5 +9,7 @@ private:
 
 public:
 	static void PengRobinsonCalculator(bool* p_open);
+	float KappaCalculation(float AcentricFactor) {0.37464 + 1.54226*AcentricFactor - 0.26992*AcentricFactor*AcentricFactor;}
+	float Alpha(float temperature, float temperatureCrit, float AcentricFactor) {return powf(1+ KappaCalculation(AcentricFactor)*(1-powf(temperature/temperatureCrit, 0.5)), 2);}
 };
 
